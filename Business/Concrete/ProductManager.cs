a45c70d15fe44927bd8f65f34060e95919a9b6f5
MUSA KAZIM <ellekli_musa@hotmail.com>
2021-02-13 21:10:25 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,23 @@
+﻿using Business.Abstract;
+using DataAccess;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public List<Product> GetAll()
+        {
+            //İş kodları
+            return _productDal.GetAll();
+        }
+    }
+}
---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //Entity implement eden class bir veri tabanı tablosudur.
+    public interface IEntity
+    {
+    }
+}
